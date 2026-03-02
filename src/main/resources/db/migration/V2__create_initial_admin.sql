bba57537b22519da8e88878a2cff20436bbc930c
Valentina Garcia <valentinagarciag089@gmail.com>
2026-03-02 18:44:20 -0500

    Fix: application.properties encoding to UTF-8

---
+++ src/main/resources/db/migration/V2__create_initial_admin.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (nombre, apellido, email, password, celular, rol)
+VALUES (
+    'Yudy',
+    'Administradora',
+    'yudy@tuapi.com',
+    '$2a$10$fE6R4T1kR5N8.X0qXn7yEeE9y7k9vQzW1o/fWv6v0m/qV6oW9uYt2',
+    '3001234567',
+    'ADMIN'
+);
