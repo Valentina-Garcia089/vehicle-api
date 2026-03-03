b270080aee24682f09226151a9ccbdcb6f756864
Valentina Garcia <valentinagarciag089@gmail.com>
2026-03-03 08:27:41 -0500

    Cambio de ruta en el securityConf

--- src/main/java/com/vehiculos/vehiculos_api/security/SecurityConfig.java
+++ src/main/java/com/vehiculos/vehiculos_api/security/SecurityConfig.java
@@ -78,7 +78,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); //react
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://vehiculos-front.vercel.app")); //react
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
